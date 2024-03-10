8386a3eaf99c525a096e567d7d359582d5d2ca88
grintzdel <mathis.oudin@etu.uca.fr>
2024-03-10 17:19:48 +0100

    Page d'accueil terminée.

--- app/index/hero.js
+++ app/index/hero.js
@@ -1,7 +1,9 @@
 // Fonction permettant de changer le contenu HTML du titre de la hero section suivant la largeur de l'écran.
 function updateH1Text() {
+    // Déclaration des variables
     let h1Element = document.querySelector('.hero__texts h1');
     let pElement = document.querySelector('.hero__texts p:last-child')
+
     if (window.innerWidth <= 1279) {
         h1Element.textContent = 'Visual Designer';
         pElement.textContent = 'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com'
